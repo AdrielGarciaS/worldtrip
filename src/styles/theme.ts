48f3ad4994140531d674a331fdeab6c376fd3b55
Adriel Garcia <adrielgarcia@live.com>
2021-06-19 15:59:15 -0300

    Installed chakra ui and setup theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      100: 'rgba(255, 186, 8, 0.5)',
+      200: '#FFBA08',
+    },
+    gray: {
+      50: '#F5F8FA',
+      100: '#DADADA',
+      200: 'rgb(153, 153, 153, 0.5)',
+      400: '#999999',
+      600: '#47585B',
+    },
+    fonts: {
+      heading: 'Poppins',
+      body: 'Poppins',
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.100',
+        color: 'gray.600',
+      },
+    },
+  },
+});
