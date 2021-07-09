78af9691c9383e3378b8db5e4387a69157763e5f
Adriel Garcia <adrielgarcia@live.com>
2021-07-08 22:37:14 -0300

    feat: created categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,36 @@
-import { VStack, Box, Flex, Text, Image } from '@chakra-ui/react';
+import {
+  VStack,
+  Box,
+  Flex,
+  Text,
+  Image,
+  HStack,
+  Center,
+  Divider,
+} from '@chakra-ui/react';
+
+const categories = [
+  {
+    title: 'vida noturna',
+    icon: '/assets/cocktail.svg',
+  },
+  {
+    title: 'praia',
+    icon: '/assets/board.svg',
+  },
+  {
+    title: 'moderno',
+    icon: '/assets/building.svg',
+  },
+  {
+    title: 'clássico',
+    icon: '/assets/museum.svg',
+  },
+  {
+    title: 'e mais...',
+    icon: '/assets/earth.svg',
+  },
+];
 
 export default function Home() {
   return (
@@ -10,8 +42,9 @@ export default function Home() {
         bgImage="url('/assets/sky-background.svg')"
         bgPosition="center"
         bgRepeat="no-repeat"
+        mb="20"
       >
-        <Flex align="flex-start" justify="space-between" pt="24" mb="20">
+        <Flex align="flex-start" justify="space-between" pt="24">
           <Box mb="5">
             <Text
               as="h2"
@@ -39,6 +72,40 @@ export default function Home() {
           <Image src="/assets/airplane.svg" alt="Avião" h="16.875rem" />
         </Flex>
       </Box>
+
+      <Box w="full" pb="20">
+        <HStack spacing="32" align="center" justify="center">
+          {categories.map(category => (
+            <VStack key={category.title} align="center">
+              <Image src={category.icon} alt={category.title} maxH="5.31rem" />
+              <Text fontWeight="600" fontSize="1.5rem">
+                {category.title}
+              </Text>
+            </VStack>
+          ))}
+        </HStack>
+      </Box>
+
+      <Divider
+        borderBottomWidth="2px"
+        width="5.625rem"
+        borderColor="gray.600"
+        opacity="1"
+        borderRadius="1px"
+      />
+
+      <Center>
+        <Text
+          textAlign="center"
+          marginBlock="14"
+          lineHeight="3.375rem"
+          fontSize="xx-large"
+          fontWeight="500"
+        >
+          Vamos nessa? <br />
+          Então escolha seu continente
+        </Text>
+      </Center>
     </VStack>
   );
 }
